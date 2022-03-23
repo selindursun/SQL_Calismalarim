540efa1821e32f875346aed63b22822ce8f45f05
103SABAH_SELİN <103SABAH_SELN@DESK6>
2022-03-23 11:30:05 +0300

    Add project files.

---
+++ SQLTablesCreateQueries.sql
@@ -0,0 +1,16 @@
+﻿-- Öğrenciler tablosu eklendi.
+
+-- Yazarlar tablosu eklendi.
+
+-- Turler tablosu eklendi.
+
+-- İşlem tablosu oluşturuldu:
+
+CREATE TABLE Islem
+(
+IslemID int IDENTITY(1,1) PRIMARY KEY,
+OgrID int NOT NULL,
+KitapID int NOT NULL,
+OduncAldigiTarih datetime NOT NULL,
+OduncBitisTarih datetime NOT NULL
+)
